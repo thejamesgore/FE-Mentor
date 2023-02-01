da1cf40869ce6c4e5dfc495714abd49cf298f50c
James Gore <83005220+thejamesgore@users.noreply.github.com>
2023-02-01 18:36:10 +0000

    Problem 02

---
+++ 02-product-preview-card-component-main/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+import imgDesk from './assets/image-product-desktop.jpg'
+import imgMob from './assets/image-product-mobile.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="image-container"></div>
+      <div className="content-container"></div>
+    </div>
+  )
+}
+
+export default App
